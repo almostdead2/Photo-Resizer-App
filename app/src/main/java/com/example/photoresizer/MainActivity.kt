34e5da2fb0c94f889854a27ce19096d047660028
almostdead2 <128363011+almostdead2@users.noreply.github.com>
2025-07-31 11:19:36 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/photoresizer/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.photoresizer
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.allowFileAccess = true
+        settings.domStorageEnabled = true
+
+        webView.loadUrl("file:///android_asset/index.html")
+        setContentView(webView)
+    }
+}
